fccd257a811a93f65e42b56ce23c4ab76b29d446
sde <simon.dellac@gmail.com>
2023-11-20 00:29:45 +0100

    Revert "re-integrate chosen_odds (0.25 or 1)" Revert "huge update for computing headliner odds" -> reverting last 2 commits before fixes of formulas

--- chosen_roll.py
+++ chosen_roll.py
@@ -44,214 +44,34 @@ def main():
         draw_chart([0], [], level, gold, False, [0])
         return
 
-    # list of tuples: (probability, nb copies in pool for desired headliner). the number of copies in pool is used to compute probability with more precision when multiple headliners are selected
-    probs_pool, probs_next_lvl_pool = [], []
+    probs, probs_next_lvl = [], []
     for cost, n_champ, n_cost, trait_odd in zip(costs, n_champs, n_costs, trait_odds):
         data_cost = data[str(cost)]
         if (data_cost["pool"] - n_champ) < data_cost["pool"] / 2:
-            probs_pool.append(0)
+            probs.append(0)
             if level < 10:
-                probs_next_lvl_pool.append(0)
+                probs_next_lvl.append(0)
         else:
-            # average number of copies in the pool of each champion of the desired cost, before draw
-            avg_champ_cost_out = data_cost["pool"] - (
-                (n_cost + n_champ) / data_cost["n_champs"]
+            prob = trait_odd * (chosen_data.iloc[level - 1].iloc[cost - 1] / 100)
+            probs.append(
+                (data_cost["pool"] - n_champ)
+                / (data_cost["n_champs"] * data_cost["pool"] - n_cost - n_champ)
+                * chosen_odds
+                * prob
             )
-            # number of champions of the desired cost in the pool, before draw
-            champ_cost_remain = (
-                data_cost["pool"] * data_cost["n_champs"] - n_champ - n_cost
-            )
-            # number of remaining copies of the desired champion in the pool, before draw
-            champ_remain = (data_cost["pool"] - n_champ) * trait_odd
 
-            champ_tier_odd = chosen_data.iloc[level - 1].iloc[cost - 1] / 100
-            champ_tier_odd_next_lvl = chosen_data.iloc[level].iloc[cost - 1] / 100
-
-            probs_pool.append(
-                compute_prob(
-                    cost,
-                    champ_remain,
-                    avg_champ_cost_out,
-                    champ_cost_remain,
-                    champ_tier_odd,
-                    data_cost["pool"],
-                ),
-            )
             if level < 10:
-                probs_next_lvl_pool.append(
-                    compute_prob(
-                        cost,
-                        champ_remain,
-                        avg_champ_cost_out,
-                        champ_cost_remain,
-                        champ_tier_odd_next_lvl,
-                        data_cost["pool"],
-                    ),
+                prob_next_lvl = trait_odd * (
+                    chosen_data.iloc[level].iloc[cost - 1] / 100
+                )
+                probs_next_lvl.append(
+                    (data_cost["pool"] - n_champ)
+                    / (data_cost["n_champs"] * data_cost["pool"] - n_cost - n_champ)
+                    * chosen_odds
+                    * prob_next_lvl
                 )
 
-    draw_chart(
-        probs_pool,
-        names,
-        level,
-        gold,
-        show_next_level,
-        chosen_odds,
-        probs_next_lvl_pool,
-    )
-
-
-"""prob to NOT find desired headliner in a draw. We use the "pity" mechanism, that prevent an headliner to be proposed twice in a row."""
-
-
-def compute_prob(
-    cost,
-    champ_remain,
-    avg_champ_cost_out,
-    champ_cost_remain,
-    champ_tier_odd,
-    pool_for_cost,
-):
-    prob = (
-        (
-            (
-                (champ_cost_remain - avg_champ_cost_out - champ_remain)
-                / (champ_cost_remain - avg_champ_cost_out)
-            )
-            * champ_tier_odd
-            + ((champ_cost_remain - champ_remain) / champ_cost_remain)
-            * (1 - champ_tier_odd)
-        )
-        * champ_tier_odd
-    ) + (1 - champ_tier_odd)
-    return prob, pool_for_cost
-
-
-def draw_chart(
-    probs_pool,
-    names,
-    level,
-    gold,
-    show_next_level,
-    chosen_odds,
-    probs_next_lvl_pool=[0],
-):
-    from math import factorial
-
-    gold_to_next_lvl = XP_TO_NEXT_LEVEL[level]
-    any_headliners = []
-    if len(names) > 1:
-        prob_all = np.prod([p[0] for p in probs_pool])
-        pools = [x[1] for x in probs_pool]
-        # dictionnary: key = total copy in pool for desired headliner ; value = number of headliner with this pool (key)
-        pools, counts = np.unique(pools, return_counts=True)
-        scaling_factor_multiple_champs_in_same_pool = np.prod(
-            [
-                (k / (k - 1)) ** (v - 1) * (k - v) / (k - 1)
-                for k, v in zip(pools, counts)
-            ]
-        )
-
-        any_headliners = [
-            {
-                "name": "Any Headliner",
-                "Probability": (
-                    1
-                    - (prob_all * scaling_factor_multiple_champs_in_same_pool)
-                    ** (i * chosen_odds / 2)
-                )
-                * 100,
-                "Golds spent": i,
-            }
-            for i in range(0, gold, 2)
-        ]
-        if gold_to_next_lvl < gold and show_next_level:
-            prob_next_lvl_all = np.prod([p[0] for p in probs_next_lvl_pool])
-
-            any_headliners += [
-                {
-                    "name": f"Any Headliner_lvl_{level + 1}",
-                    "Probability": 0,
-                    "Golds spent": i,
-                }
-                for i in range(0, gold_to_next_lvl, 2)
-            ]
-            any_headliners += [
-                {
-                    "name": f"Any Headliner_lvl_{level + 1}",
-                    "Probability": (
-                        1
-                        - (
-                            (
-                                prob_next_lvl_all
-                                * scaling_factor_multiple_champs_in_same_pool
-                            )
-                            ** ((i * chosen_odds - gold_to_next_lvl) / 2)
-                        )
-                    )
-                    * 100,
-                    "Golds spent": i,
-                }
-                for i in range(gold_to_next_lvl, gold, 2)
-            ]
-
-    headliners = any_headliners
-    for i, (prob_pool, name) in enumerate(zip(probs_pool, names)):
-        headliners += [
-            {
-                "name": name,
-                "Probability": (1 - prob_pool[0] ** (i * chosen_odds / 2)) * 100,
-                "Golds spent": i,
-            }
-            for i in range(0, gold, 2)
-        ]
-
-    if gold_to_next_lvl < gold and show_next_level:
-        for i, (prob_pool, name) in enumerate(zip(probs_next_lvl_pool, names)):
-            headliners += [
-                {
-                    "name": name + f"_lvl_{level + 1}",
-                    "Probability": 0,
-                    "Golds spent": i,
-                }
-                for i in range(0, gold_to_next_lvl, 2)
-            ]
-            headliners += [
-                {
-                    "name": name + f"_lvl_{level + 1}",
-                    "Probability": (
-                        1 - prob_pool[0] ** ((i * chosen_odds - gold_to_next_lvl) / 2)
-                    )
-                    * 100,
-                    "Golds spent": i,
-                }
-                for i in range(gold_to_next_lvl, gold, 2)
-            ]
-
-    df = pd.DataFrame(headliners)
-    if not df.empty:
-        fig = px.line(
-            df,
-            y="Probability",
-            x="Golds spent",
-            title="Odds to find one of the desired Headliners",
-            color="name",
-        )
-        fig.update_layout(
-            yaxis=dict(range=[0, 100]),
-            height=550,
-            width=900,
-            xaxis={"tickmode": "linear", "dtick": 4, "range": [0, gold]},
-            hovermode="x",
-            title_x=0.4,
-            legend_title=None,
-        )
-        fig.update_traces(
-            hovertemplate="Probability: <b>%{y:.2f}</b>%<br>Golds spent: <b>%{x}</b>",
-            text="",
-            mode="lines",
-        )
-
-        st.write(fig)
+    draw_chart(probs, names, level, gold, show_next_level, probs_next_lvl)
 
 
 def select_params_chosen(data, chosen_data, traits):
@@ -265,6 +85,10 @@ def select_params_chosen(data, chosen_data, traits):
 
     # Gold
     gold = st.sidebar.slider("Gold", value=100, min_value=1, max_value=150)
+
+    chosen_odds = list(chosen_data.iloc[level - 1])
+    # available_costs = [i + 1 for i in range(5) if chosen_odds[i] > 0]
+    # available_traits = traits[traits["cost"].isin(available_costs)]
 
     has_chosen = st.sidebar.checkbox("Already possess an Headliner?")
     show_next_level = st.sidebar.checkbox("Show next level odds?")
@@ -316,6 +140,98 @@ def select_params_chosen(data, chosen_data, traits):
         gold,
         show_next_level,
     )
+
+
+def draw_chart(probs, names, level, gold, show_next_level, probs_next_lvl=[0]):
+    gold_to_next_lvl = XP_TO_NEXT_LEVEL[level]
+    any_headliners = []
+    if len(names) > 1:
+        prob_all = np.prod([1 - p for p in probs])
+        any_headliners = [
+            {
+                "name": "Any Headliner",
+                "Probability": (1 - prob_all ** (i / 2)) * 100,
+                "Golds spent": i,
+            }
+            for i in range(0, gold, 2)
+        ]
+        if gold_to_next_lvl < gold and show_next_level:
+            prob_next_lvl = np.prod([1 - p for p in probs_next_lvl])
+
+            any_headliners += [
+                {
+                    "name": f"Any Headliner_lvl_{level + 1}",
+                    "Probability": 0,
+                    "Golds spent": i,
+                }
+                for i in range(0, gold_to_next_lvl, 2)
+            ]
+            any_headliners += [
+                {
+                    "name": f"Any Headliner_lvl_{level + 1}",
+                    "Probability": (1 - (prob_next_lvl ** ((i - gold_to_next_lvl) / 2)))
+                    * 100,
+                    "Golds spent": i,
+                }
+                for i in range(gold_to_next_lvl, gold, 2)
+            ]
+
+    headliners = any_headliners
+    for i, (prob, name) in enumerate(zip(probs, names)):
+        headliners += [
+            {
+                "name": name,
+                "Probability": (1 - (1 - prob) ** (i / 2)) * 100,
+                "Golds spent": i,
+            }
+            for i in range(0, gold, 2)
+        ]
+
+    if gold_to_next_lvl < gold and show_next_level:
+        for i, (prob, name) in enumerate(zip(probs_next_lvl, names)):
+            headliners += [
+                {
+                    "name": name + f"_lvl_{level + 1}",
+                    "Probability": 0,
+                    "Golds spent": i,
+                }
+                for i in range(0, gold_to_next_lvl, 2)
+            ]
+            headliners += [
+                {
+                    "name": name + f"_lvl_{level + 1}",
+                    "Probability": (1 - (1 - prob) ** ((i - gold_to_next_lvl) / 2))
+                    * 100,
+                    "Golds spent": i,
+                }
+                for i in range(gold_to_next_lvl, gold, 2)
+            ]
+
+    df = pd.DataFrame(headliners)
+    if not df.empty:
+        fig = px.line(
+            df,
+            y="Probability",
+            x="Golds spent",
+            title="Odds to find one of the desired Headliners",
+            color="name",
+        )
+        fig.update_layout(
+            yaxis=dict(range=[0, 100]),
+            height=550,
+            width=900,
+            xaxis={"tickmode": "linear", "dtick": 4, "range": [0, gold]},
+            hovermode="x",
+            title_x=0.4,
+            legend_title=None,
+        )
+        fig.update_traces(
+            hovertemplate="Probability: <b>%{y:.2f}</b>%<br>Golds spent: <b>%{x}</b>",
+            text="",
+            mode="lines",
+        )
+
+        st.write(fig)
 
 
 def build_champ_select(traits, level):
